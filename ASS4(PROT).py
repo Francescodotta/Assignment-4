7409ce726bac7914f0e1d0a13dbb80c4c255897d
Francescodotta <45691667+Francescodotta@users.noreply.github.com>
2019-01-14 16:34:24 +0100

    Add files via upload

---
+++ ASS4(PROT).py
@@ -0,0 +1,29 @@
+CodonsRNA = {
+    'UUU': 'F',      'CUU': 'L',      'AUU': 'I',      'GUU': 'V',
+    'UUC': 'F',      'CUC': 'L',      'AUC': 'I',      'GUC': 'V',
+    'UUA': 'L',      'CUA': 'L',      'AUA': 'I',      'GUA': 'V',
+    'UUG': 'L',      'CUG': 'L',      'AUG': 'M',      'GUG': 'V',
+    'UCU': 'S',      'CCU': 'P',      'ACU': 'T',      'GCU': 'A',
+    'UCC': 'S',      'CCC': 'P',      'ACC': 'T',      'GCC': 'A',
+    'UCA': 'S',      'CCA': 'P',      'ACA': 'T',      'GCA': 'A',
+    'UCG': 'S',      'CCG': 'P',      'ACG': 'T',      'GCG': 'A',
+    'UAU': 'Y',      'CAU': 'H',      'AAU': 'N',      'GAU': 'D',
+    'UAC': 'Y',      'CAC': 'H',      'AAC': 'N',      'GAC': 'D',
+    'UAA': '\n',     'CAA': 'Q',      'AAA': 'K',      'GAA': 'E',
+    'UAG': '\n',     'CAG': 'Q',      'AAG': 'K',      'GAG': 'E',
+    'UGU': 'C',      'CGU': 'R',      'AGU': 'S',      'GGU': 'G',
+    'UGC': 'C',      'CGC': 'R',      'AGC': 'S',      'GGC': 'G',
+    'UGA': '\n',     'CGA': 'R',      'AGA': 'R',      'GGA': 'G',
+    'UGG': 'W',      'CGG': 'R',      'AGG': 'R',      'GGG': 'G'}
+with open('/Users/francescodotta/Desktop/rosalind_prot.txt') as file:
+    rna = file.readline().strip()
+def trans(rna):
+    Prot = ''
+    StopCodons = ['UAA','UAG','UGA']
+    for index in (range(len(rna)//3)):
+        codon = rna[3*index:(3*index)+3]
+        if codon in StopCodons:
+            return Prot
+        Prot = Prot + CodonsRNA[codon]
+    return Prot
+print(trans(rna))
